f414d0a0e24f6ab43b28826765f5fafe3498c59a
Rayzen7 <nugrahasetio6@gmail.com>
2025-08-31 11:19:46 +0700

    update Auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request) 
+    {
         try {
             $isAny = User::where('username', $request)->first();
 
@@ -25,14 +28,45 @@ class UserController extends Controller
             $user->save();
 
             return response()->json([
-                'message' => 'Register successfully!',
-                'user' => $user
+                'message' => 'Daftar Berhasil!',
+                'data' => $user
             ], 201);
 
         } catch(Exception $err) {
             return response()->json([
                 'message' => $err->getMessage()
-            ]);
+            ], 422);
         }
+    }
+
+    public function login(Request $request) 
+    {
+        try {
+            $user = User::where("username", $request->username)->first();
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    "message" => "Nama atau Kata Sandi Salah!"
+                ], 401);
+            }
+            
+            $token = $user->createToken("access_token")->plainTextToken;
+            return response()->json([
+                "message" => "Masuk Berhasil!",
+                "token" => $token,
+                "data" => $user
+            ]);
+        } catch (Exception $err) {
+            return response()->json([
+                "message" => $err->getMessage()
+            ], 422);
+        }
+    }
+
+    public function me() 
+    {
+        $user = Auth::user();
+        return response()->json([
+            "data" => $user,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth
+Route::post("/register", [UserController::class, 'register']);
+Route::post("/login", [UserController::class, 'login']);
+
+Route::middleware("auth")->group(function() {
+    Route::get("/me", [UserController::class, 'me']);
+});
